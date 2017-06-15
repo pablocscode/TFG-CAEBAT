d3fac097b195372bdbac579d916ae3aeafeef521
pablocscode <pablocastrosant@gmail.com>
2017-06-15 04:14:26 -0400

    Add files via upload

--- representacion1.0.py
+++ representacion1.0.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Leemos todas las líneas del archivo
-archivo = open('profiles.txt','r')
+archivo = open('profiles.out','r')
 lineas = archivo.readlines()
 archivo.close()
 
@@ -23,7 +23,7 @@ while lineas[i] != '  \n':
     i += 1
 
 #Calculamos el número de tiempos del archivo
-datos_halfcells = open('halfcells.txt','r')
+datos_halfcells = open('halfcells.out','r')
 lineas_halfcells = datos_halfcells.readlines()
 datos_halfcells.close()
 num_tiempos = len(lineas_halfcells)-1 #la primera linea no tiene datos
@@ -76,22 +76,25 @@ def plot(numero):
     plt.xlabel('Distancia')
 
     plt.figure(2)
-    plt.plot(distancia[numero],C_Sol_Surf[numero],'.')
+    plt.plot(distancia[numero],C_Sol_Surf[numero],'o')
+    plt.plot(distancia[0],C_Sol_Surf[0],'o')
     plt.ylabel('Concentración Sólido')
     plt.xlabel('Distancia')
     plt.title(tiempo[numero])
 
     plt.figure(3)
-    plt.plot(distancia[numero],Liq_Pot[numero],'.')
+    plt.plot(distancia[numero],Liq_Pot[numero],'o')
+    plt.plot(distancia[0],Liq_Pot[0],'o')
     plt.ylabel('Potencial en el líquido')
     plt.xlabel('Distancia')
     plt.title(tiempo[numero])
 
     plt.figure(4)
-    plt.plot(distancia[numero],Solid_Pot[numero],'.')
+    plt.plot(distancia[numero],Solid_Pot[numero],'o')
+    plt.plot(distancia[0],Solid_Pot[0],'o')
     plt.ylabel('Potencial en el sólido')
     plt.xlabel('Distancia')
-    plt.title((tiempo[numero])
+    plt.title(('Tiempo =', tiempo[numero],' min'))
 """
     plt.figure(5)
     plt.plot(distancia[numero],J_main[numero],'o')
